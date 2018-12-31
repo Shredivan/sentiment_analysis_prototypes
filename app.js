835af873c73b495d21e5978e4f91091e57fa018d
David Sheridan <davidsheridandsgn@gmail.com>
2018-12-31 14:15:19 +0000

    toggle results and clear query
    functionality of buttons added

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
-  console.log("testing");
-)
+  $('.btn-primary').click(function(){
+
+    // Check text area has content before showing results
+    //swap out id for class later or conflicts will emerge with other html files
+    if($('#exampleFormControlTextarea1').val() !== ''){
+      $('.results-content').css('visibility', 'visible')
+    }
+  })
+  $('.btn-secondary').click(function(){
+    $('#exampleFormControlTextarea1').val('')
+  })
+});
