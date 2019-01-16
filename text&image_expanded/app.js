0fc5a25321d465d08fd9035e5f8fc65192b516ae
David <david@SYFL-MLPT-048.local>
2019-01-16 18:30:06 +0000

    add test highchart to document

--- text&image_expanded/app.js
+++ text&image_expanded/app.js
@@ -13,14 +13,39 @@ $(document).ready(function(){
       $('.results-content').show()
       $('.no-text-warning-area').hide()
     }
-  })
+  });
 
   $('.btn-secondary').click(function(){
     $('#text_truncated').val('')
-  })
+  });
 
   $('.page-title-link').click(function(){
     location.reload()
-  })
+  });
 
+  $(function () {
+    var myChart = Highcharts.chart('treemap-container', {
+        chart: {
+            type: 'bar'
+        },
+        title: {
+            text: 'Fruit Consumption'
+        },
+        xAxis: {
+            categories: ['Apples', 'Bananas', 'Oranges']
+        },
+        yAxis: {
+            title: {
+                text: 'Fruit eaten'
+            }
+        },
+        series: [{
+            name: 'Jane',
+            data: [1, 0, 4]
+        }, {
+            name: 'John',
+            data: [5, 7, 3]
+        }]
+    });
+  });
 });
