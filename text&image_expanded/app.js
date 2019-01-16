743e591315c16f404556fa3d18f4e3eed09661d8
David <david@SYFL-MLPT-048.local>
2019-01-16 18:43:13 +0000

    add highchart treemap

--- text&image_expanded/app.js
+++ text&image_expanded/app.js
@@ -24,28 +24,82 @@ $(document).ready(function(){
   });
 
   $(function () {
-    var myChart = Highcharts.chart('treemap-container', {
-        chart: {
-            type: 'bar'
-        },
-        title: {
-            text: 'Fruit Consumption'
-        },
-        xAxis: {
-            categories: ['Apples', 'Bananas', 'Oranges']
-        },
-        yAxis: {
-            title: {
-                text: 'Fruit eaten'
+    Highcharts.chart('treemap-container', {
+    series: [{
+        type: "treemap",
+        layoutAlgorithm: 'stripes',
+        alternateStartingDirection: true,
+        levels: [{
+            level: 1,
+            layoutAlgorithm: 'sliceAndDice',
+            dataLabels: {
+                enabled: true,
+                align: 'left',
+                verticalAlign: 'top',
+                style: {
+                    fontSize: '15px',
+                    fontWeight: 'bold'
+                }
             }
-        },
-        series: [{
-            name: 'Jane',
-            data: [1, 0, 4]
+        }],
+        data: [{
+            id: 'A',
+            name: 'Apples',
+            color: "#EC2500"
         }, {
-            name: 'John',
-            data: [5, 7, 3]
+            id: 'B',
+            name: 'Bananas',
+            color: "#ECE100"
+        }, {
+            id: 'O',
+            name: 'Oranges',
+            color: '#EC9800'
+        }, {
+            name: 'Anne',
+            parent: 'A',
+            value: 5
+        }, {
+            name: 'Rick',
+            parent: 'A',
+            value: 3
+        }, {
+            name: 'Peter',
+            parent: 'A',
+            value: 4
+        }, {
+            name: 'Anne',
+            parent: 'B',
+            value: 4
+        }, {
+            name: 'Rick',
+            parent: 'B',
+            value: 10
+        }, {
+            name: 'Peter',
+            parent: 'B',
+            value: 1
+        }, {
+            name: 'Anne',
+            parent: 'O',
+            value: 1
+        }, {
+            name: 'Rick',
+            parent: 'O',
+            value: 3
+        }, {
+            name: 'Peter',
+            parent: 'O',
+            value: 3
+        }, {
+            name: 'Susanne',
+            parent: 'Kiwi',
+            value: 2,
+            color: '#9EDE00'
         }]
+      }],
+      title: {
+          text: 'Fruit consumption'
+      }
     });
   });
 });
