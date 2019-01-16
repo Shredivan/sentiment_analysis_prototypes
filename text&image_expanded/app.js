662d36f0173c9f0c3437df92fc476a472c74ccb9
David <david@SYFL-MLPT-048.local>
2019-01-16 18:49:21 +0000

    add correct colours and remove redundant dummy data

--- text&image_expanded/app.js
+++ text&image_expanded/app.js
@@ -44,57 +44,28 @@ $(document).ready(function(){
         }],
         data: [{
             id: 'A',
-            name: 'Apples',
-            color: "#EC2500"
+            name: 'Positive',
+            color: "#33dca5"
         }, {
             id: 'B',
-            name: 'Bananas',
-            color: "#ECE100"
+            name: 'Neutral',
+            color: "#f3d333"
         }, {
             id: 'O',
-            name: 'Oranges',
-            color: '#EC9800'
+            name: 'Negative',
+            color: '#cc2f3d'
         }, {
-            name: 'Anne',
+            name: '',
             parent: 'A',
             value: 5
         }, {
-            name: 'Rick',
-            parent: 'A',
-            value: 3
-        }, {
-            name: 'Peter',
-            parent: 'A',
-            value: 4
-        }, {
-            name: 'Anne',
+            name: '',
             parent: 'B',
             value: 4
         }, {
-            name: 'Rick',
-            parent: 'B',
-            value: 10
-        }, {
-            name: 'Peter',
-            parent: 'B',
-            value: 1
-        }, {
-            name: 'Anne',
+            name: '',
             parent: 'O',
-            value: 1
-        }, {
-            name: 'Rick',
-            parent: 'O',
-            value: 3
-        }, {
-            name: 'Peter',
-            parent: 'O',
-            value: 3
-        }, {
-            name: 'Susanne',
-            parent: 'Kiwi',
-            value: 2,
-            color: '#9EDE00'
+            value: 4
         }]
       }],
       title: {
