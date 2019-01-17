8c4a88c8d7ba038159f2f7f851ac5d9dc09e33db
David <david@SYFL-MLPT-048.local>
2019-01-17 17:32:31 +0000

    remove highcharts title

--- text&image_expanded/app.js
+++ text&image_expanded/app.js
@@ -71,7 +71,10 @@ $(document).ready(function(){
         }]
       }],
       title: {
-          text: 'Fruit consumption'
+          text: 'Fruit consumption',
+          style:{
+            display: 'none'
+          }
       }
     });
   });
